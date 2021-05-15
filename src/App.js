a100edaf9f168083a882594a427738add08f0c1b
cristian <cristian.ortiz@uda.cl>
2021-05-15 19:47:19 -0400

    setup of app components like products, header  and style the basic elements for the frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Products from "./components/Products";
+import NewProduct from "./components/NewProduct";
+import EditProduct from "./components/EditProduct";
+
 function App() {
-  return <h1>Redux</h1>;
+  //the content inside Switch comp, will be shown in every app pages
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/products/new" component={NewProduct} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
